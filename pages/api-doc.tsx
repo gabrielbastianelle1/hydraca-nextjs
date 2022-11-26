480d95bf9226889a1223df238175329bb5675b9e
Gabriel Bastianelle <gsbastianelle@gmail.com>
2022-11-26 19:38:19 +0000

    swagger configured

--- pages/api-doc.tsx
+++ pages/api-doc.tsx
@@ -1,32 +1,114 @@
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
-import { createSwaggerSpec } from 'next-swagger-doc';
-import dynamic from 'next/dynamic';
-import 'swagger-ui-react/swagger-ui.css';
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
+import { createSwaggerSpec } from 'next-swagger-doc'
+import dynamic from 'next/dynamic'
+import 'swagger-ui-react/swagger-ui.css'
 
 const SwaggerUI = dynamic<{
-  spec: any;
-}>(import('swagger-ui-react'), { ssr: false });
+    spec: any
+}>(import('swagger-ui-react'), { ssr: false })
 
 function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <SwaggerUI spec={spec} />;
+    return <SwaggerUI spec={spec} />
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const spec: Record<string, any> = createSwaggerSpec({
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Next Swagger API Example',
-        version: '1.0',
-      },
-    },
-  });
+    const spec: Record<string, any> = createSwaggerSpec({
+        definition: {
+            openapi: '3.0.0',
+            info: {
+                title: 'HYDRACA API DOCUMENTATION',
+                version: '1.0.0',
+                description: 'GITHUB REPOSITORY: [HYDRACA repository](https://github.com/gabrielbastianelle1/hydraca-nextjs)'
+            },
+            tags: [
+                {
+                    name: 'auth',
+                    description: 'everything about signin and signup'
+                },
+                {
+                    name: 'user',
+                    description: 'everything about user'
+                },
+                {
+                    name: 'admin',
+                    description: 'everything about admin'
+                }
+            ],
+            components:{
+                schemas:{
+                    User:{
+                        type: 'object',
+                        properties: {
+                            id: {
+                                type: 'integer'
+                            },
+                            role: {
+                                type: 'string',
+                                description: 'if the user is admin or not',
+                                enum: [
+                                    'admin',
+                                    'user'
+                                ]
+                            },
+                            name: {
+                                type: 'string'
+                            },
+                            email: {
+                                type: 'string'
+                            },
+                            password: {
+                                type: 'string'
+                            },
+                            birthday: {
+                                type: 'string',
+                                format: 'data-time'
+                            },
+                            state: {
+                                type: 'boolean'
+                            },
+                            height: {
+                                type: 'integer'
+                            },
+                            weight: {
+                                type: 'integer'
+                            },
+                            imc: {
+                                type: 'integer'
+                            },
+                            sensitivity: {
+                                type: 'integer'
+                            },
+                            glucoserange: {
+                                type: 'array',
+                                items: {
+                                    type: 'integer',
+                                },
+                                description: 'array with to positions only, the fist is the minimum glucose, and second maximum'
+                            }
+                        }
+                    },
+                    Register: {
 
-  return {
-    props: {
-      spec,
-    },
-  };
-};
+                    },
+                    Diabetes: {
 
-export default ApiDoc;
+                    },
+                    Therapy: {
+
+                    },
+                    Food: {
+
+                    }
+                }
+            }
+
+        }
+    })
+    return {
+        props: {
+            spec
+        }
+    }
+}
+
+export default ApiDoc
