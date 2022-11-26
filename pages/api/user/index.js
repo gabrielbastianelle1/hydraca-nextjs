91f913604c389525fd7f8702fb2b63d23548d6e2
Gabriel Bastianelle <gsbastianelle@gmail.com>
2022-11-26 03:21:42 +0000

    swagger

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -1,4 +1,14 @@
 const userController = require('../../../src/controllers/controller.user')
 const verifyToken = require('../../../src/middlewares/verifyToken')
 
+/**
+ * @swagger
+ * /api/user:
+ *   get:
+ *     description: verify token and return all data from user recurrent that token,
+ *     responses:
+ *       200:
+ *         description: json with user data
+ */
+
 export default verifyToken(userController.getCurrentUser)
