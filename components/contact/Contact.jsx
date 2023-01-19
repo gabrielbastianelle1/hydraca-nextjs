fa07aea5241c517cc6104285b42b02aaedfed916
Vivian Martins <64610367+vivianmartins@users.noreply.github.com>
2023-01-19 11:20:54 +0000

    conact warningt

--- components/contact/Contact.jsx
+++ components/contact/Contact.jsx
@@ -6,8 +6,8 @@ export default function Contact() {
     const h2 = useRef(null)
     return (
         <main>
-            {/**     <div className="flex h-screen justify-center p-24 ">
-                <div className="square">
+            {/**     <div classNameName="flex h-screen justify-center p-24 ">
+                <div classNameName="square">
                     <h1 className="uppercase text-center animate__animated animate__pulse  font-bold text-2xl text-colorButton">
                         Sobre o projeto
                     </h1>
@@ -30,86 +30,86 @@ export default function Contact() {
                         data-wow-duration="1s"
                         data-wow-delay=".1s"
                     >
-                        <div class="container my-9 lg:my-20 lg:px-6 lg:mx-44">
-                            <div class="flex justify-center">
-                                <div class="flex justify-items-center">
-                                    <div class="grow-0 shrink-0 basis-auto w-full lg:w-7/12">
-                                        <div class="flex flex-wrap">
-                                            <div class="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
-                                                <div class="flex items-start">
-                                                    <div class="shrink-0">
-                                                        <div class="p-4 bg-blue-900 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                        <div className="container my-9 lg:my-20 lg:px-6 lg:mx-44">
+                            <div className="flex justify-center">
+                                <div className="flex justify-items-center">
+                                    <div className="grow-0 shrink-0 basis-auto w-full lg:w-7/12">
+                                        <div className="flex flex-wrap">
+                                            <div className="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+                                                <div className="flex items-start">
+                                                    <div className="shrink-0">
+                                                        <div className="p-4 bg-blue-900 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
                                                             <Suport />
                                                         </div>
                                                     </div>
-                                                    <div class="grow ml-6">
-                                                        <p class="font-bold mb-1">
+                                                    <div className="grow ml-6">
+                                                        <p className="font-bold mb-1">
                                                             Suporte Técnico
                                                         </p>
-                                                        <p class="text-gray-500">
+                                                        <p className="text-gray-500">
                                                             suport@example.com
                                                         </p>
-                                                        <p class="text-gray-500">
+                                                        <p className="text-gray-500">
                                                             99999999
                                                         </p>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
-                                                <div class="flex items-start">
-                                                    <div class="shrink-0">
-                                                        <div class="p-4 bg-blue-900 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                                            <div className="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+                                                <div className="flex items-start">
+                                                    <div className="shrink-0">
+                                                        <div className="p-4 bg-blue-900 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
                                                             <Question />
                                                         </div>
                                                     </div>
-                                                    <div class="grow ml-6">
-                                                        <p class="font-bold mb-1">
+                                                    <div className="grow ml-6">
+                                                        <p className="font-bold mb-1">
                                                             Dúvidas
                                                         </p>
-                                                        <p class="text-gray-500">
+                                                        <p className="text-gray-500">
                                                             duvida@example.com
                                                         </p>
-                                                        <p class="text-gray-500">
+                                                        <p className="text-gray-500">
                                                             99988
                                                         </p>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
-                                                <div class="flex align-start">
-                                                    <div class="shrink-0">
-                                                        <div class="p-4 bg-blue-900 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                                            <div className="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+                                                <div className="flex align-start">
+                                                    <div className="shrink-0">
+                                                        <div className="p-4 bg-blue-900 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
                                                             <Geral />
                                                         </div>
                                                     </div>
-                                                    <div class="grow ml-6">
-                                                        <p class="font-bold mb-1">
+                                                    <div className="grow ml-6">
+                                                        <p className="font-bold mb-1">
                                                             Geral
                                                         </p>
-                                                        <p class="text-gray-500">
+                                                        <p className="text-gray-500">
                                                             geral@example.com
                                                         </p>
-                                                        <p class="text-gray-500">
+                                                        <p className="text-gray-500">
                                                             9999
                                                         </p>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
-                                                <div class="flex align-start">
-                                                    <div class="shrink-0">
-                                                        <div class="p-4 bg-blue-900 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                                            <div className="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+                                                <div className="flex align-start">
+                                                    <div className="shrink-0">
+                                                        <div className="p-4 bg-blue-900 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
                                                             <Bugs />
                                                         </div>
                                                     </div>
-                                                    <div class="grow ml-6">
-                                                        <p class="font-bold mb-1">
+                                                    <div className="grow ml-6">
+                                                        <p className="font-bold mb-1">
                                                             Reportar bugs
                                                         </p>
-                                                        <p class="text-gray-500">
+                                                        <p className="text-gray-500">
                                                             bugs@example.com
                                                         </p>
-                                                        <p class="text-gray-500">
+                                                        <p className="text-gray-500">
                                                             88788
                                                         </p>
                                                     </div>
