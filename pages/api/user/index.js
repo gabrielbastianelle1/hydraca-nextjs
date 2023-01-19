0ff4ed904e62739a0657f44259f9d9ef95cf401d
Vivian Martins <64610367+vivianmartins@users.noreply.github.com>
2023-01-19 22:07:00 +0000

    swagger alguns

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -2,20 +2,32 @@ const userController = require('../../../src/controllers/controller.user')
 const verifyToken = require('../../../src/middlewares/verifyToken')
 /**
  * @swagger
- * /api/index:
- *  -user
- *   get:
- *     description: verify token and return all data from user recurrent that token,
- *     responses:
- *       200:
- *    content:
- *            application/json:
- *              schema:
- *                type: array
- *                items:
- *                  $ref: '#/src/models/model.User'
- *
- *         description: json with user data
+ * paths:
+ *  /api/index:
+ *     get:
+ *      tags:
+ *              - user
+ *          sumarry: profile  users
+ *          description:  profile  users
+ *          operationId:  index
+ *          responses:
+ *              '200':
+ *                  description: succesful operation
+ *                  content:
+ *                      application/json:
+ *                          examples:
+ *                              registers:
+ *                                  value:
+ *                                     _id: '639c8f85b78834dfde9f4831'
+ *                                     role: 'user'
+ *                                     name: 'Barbara'
+ *                                     birthday: '2002-07-10'
+ *                                     email: 'barbara@gmail.com'
+ *                                     password: '$2a$10$gGpkJtPRkN01t1gQK5XXKeet7WMe9KzIf2/RYAgErbPXrJ89wdyF2'
+ *                                     state: true
+ *                                     iat: '1671204748'
+ *              '500':
+ *                  description: server error
  *
  */
 export default verifyToken(userController.getCurrentUser)
